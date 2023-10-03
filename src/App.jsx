40604291a9694cb1288af039c4b7a55dce6552ed
Erfan Asadi <asadi.love7981@gmail.com>
2023-10-03 16:49:36 +0330

    init repo and use first button component in app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return <>
+    <Button>Click Man</Button>
+  </>
+}
+
+export default App
